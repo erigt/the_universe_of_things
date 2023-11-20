9f7a4c685069a519f03fd6d4cd94f9f09971c124
Erika <146768635+erigt@users.noreply.github.com>
2023-11-20 15:03:18 +0100

    setting folders

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -2,14 +2,10 @@
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 import Pagination from '../components/Pagination.vue';
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <Header.vue></Header.vue>
   <Footer.vue></Footer.vue>
-  <Pagination.vue></Pagination.vue>
-  <main>
-    <TheWelcome />
-  </main>
+	<Pagination.vue></Pagination.vue>
 </template>
