1e5c5739ef6c318767737d66929c9afe34e44708
Sara Lorenzo <sara00lorenzo@gmail.com>
2023-11-27 14:57:55 +0100

    Added Footer html and linked scss

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,24 @@
 <script></script>
 <template>
-	<div>
-		
-	</div>
+	<footer>
+		<div class="SocialMediaIconsContainer">
+			<a href="https://github.com/">
+				<img src="" alt="GitHub icon">
+			</a>
+			<a href="https://www.facebook.com/">
+				<img src="" alt="Facebook icon">
+			</a>
+			<a href="https://www.instagram.com/">
+				<img src="" alt="Instagram icon">
+			</a>
+			<a href="https://www.youtube.com/">
+				<img src="" alt="Youtube icon">
+			</a>
+		</div>
+		<div class="CopyrightContainer">
+			<p>Prohibido el paso a toda persona ajena a la obra - 2023 ©</p>
+		</div>
+	</footer>
 </template>
+<style lang="scss" scoped>
+</style>
