238b4380b3c46487c482629019c786584dbeaeeb
Sara Lorenzo <sara00lorenzo@gmail.com>
2023-11-27 15:53:09 +0100

    deleted unused imports and fixed used ones paths

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
 import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+//import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
  <Header/>
- <Pagination/>
- <Login/>
  <Footer/>
 </template>
 <style lang="scss" scoped>
