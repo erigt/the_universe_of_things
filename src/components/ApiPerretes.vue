eae1bf9fdc37f6db5b4c94bec8c3940c8ec73dc3
CarlosUribe225 <carurbe225@gmail.com>
2023-11-28 12:39:47 +0100

    created component ApiPerretes.vue

---
+++ src/components/ApiPerretes.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Esta es el componente ApiPerretes</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,11 +1,14 @@
 <script setup>
-import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 import Pagination from '@/components/Pagination.vue';
+import ApiPerretes from '@/components/ApiPerretes.vue'
+
 </script>
 
 <template>
   <Header/>
   <Pagination/>
+  <ApiPerretes/>
   <Footer/>
 </template>
