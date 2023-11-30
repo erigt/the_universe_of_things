4763d99125762dbc437d264db6e682a2a215f19e
Matilde García Suárez <matigarcia0817@gmail.com>
2023-11-30 15:34:13 +0100

    changes necesary

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,14 @@ import Footer from '@/components/Footer.vue';
 import Pagination from '@/components/Pagination.vue';
 
 
+
 </script>
  
-</script>
+
 <template>
+  <button>Favoritos</button>
+  
+
   
  <Header/>
  <Pagination/>
