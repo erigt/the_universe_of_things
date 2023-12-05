8881c73d43a50f0c9f7cbc360787141c08ca0ffd
Natalia GF <natimetal83@gmail.com>
2023-12-05 15:34:11 +0100

    fixed selectbreed

--- src/components/DogsDropdown.vue
+++ src/components/DogsDropdown.vue
@@ -9,16 +9,16 @@ export default {
     };
   },
   mounted() {
-    
+
     this.getDogBreeds();
   },
   methods: {
     async getDogBreeds() {
       try {
-        
-        const response = await this.$axios.get('https://api.thedogapi.com/v1/breeds');
-        
-        
+
+        const response = await this.$axios.get('https://dog.ceo/api/breeds/list/all');
+
+
         this.dogBreeds = response.data.map(breed => breed.name);
       } catch (error) {
         console.error('Error al obtener las razas de perros:', error);
@@ -28,13 +28,35 @@ export default {
 };
 </script>
 <template>
-    <div>
-      <label for="dogBreed">Selecciona una raza :</label>
-      <select id="dogBreed" v-model="selectedBreed">
-        <option v-for="breed in dogBreeds" :key="breed" :value="breed">{{ breed }}</option>
-      </select>
-    </div>
-  </template>
-  <style scoped>
-  /* Agrega estilos según sea necesario */
-  </style>
+  <div>
+    <label for="dogBreed">Selecciona una raza :</label>
+    <select id="dogBreed" v-model="selectedBreed">
+      <option v-for="breed in dogBreeds" :key="breed" :value="breed">{{ breed }}</option>
+    </select>
+  </div>
+</template>
+<style scoped>
+div {
+  margin: 20px;
+}
+
+
+label {
+  display: flex;
+  margin-bottom: 8px;
+}
+
+select {
+  width: 30%;
+  padding: 8px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+
+}
+
+select :hover {
+  color: bisque;
+}
+</style>
