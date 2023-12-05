8c3e59f036e4478a6dfe3487544a1f61802cb621
Erika <146768635+erigt@users.noreply.github.com>
2023-12-05 14:57:26 +0100

    new changes styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,8 +29,12 @@ onBeforeMount(async () => {
     <main>
 
         <div class="card" v-for="dog in dogs">
-            <h4 class="breed">{{ dog.breed }}</h4>
+            <p class="breed">{{ dog.breed }}</p>
+        <div class="container-img">
             <img class="img" v-bind:src="dog.image" :alt="dog.breed" srcset="">
+            <img class="heart" src="/src/components/icons/Image20231205125028.png" alt="corazon">
+            <img class="heart2" src="/src/components/icons/corazon.png" alt="corazón">
+        </div>
         </div>
 
     </main>
@@ -41,6 +45,11 @@ onBeforeMount(async () => {
 
 main {
     background-color:#F4C3A0;
+    gap: 1rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
+    padding: 0.5%;
+   
 }
 .breed {
     font-family: Arial, Helvetica, sans-serif;
@@ -51,18 +60,40 @@ main {
 .img {
     height: 200px;
     width: 200px;
+    align-items: center;
+}
+p  {
+    margin-block-start: 0.5% !important;
+    margin-block-end:0.5% !important;
 }
 
-.card {
-    display: grid;
-	grid-template-columns: 25px 25px 25px;
-	grid-template-rows: 30px 30px 30px;
-	gap: 2rem;
-	justify-content: center;
-	align-items: center;
-	margin: 3%;
-	padding: 5%;
+.heart {
+    height: 25px;
+    width: 25px;
+    padding: 0.5%;
+
 }
+.heart2 {
+    height: 27.5px;
+    width: 27.5px;
+    display: none;
+
+}
+.container-img {
+    display: flex;
+    justify-content: end;
+    align-items: end;
+
+}
+
+ .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 0.5%;
+
+} 
 
 
 </style>
