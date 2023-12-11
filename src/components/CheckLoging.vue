3a99efdf7b22832f5a2af9f0073b803db58497b1
Matilde García Suárez <matigarcia0817@gmail.com>
2023-12-11 21:56:26 +0100

    news changes heart favorites

--- src/components/CheckLoging.vue
+++ src/components/CheckLoging.vue
@@ -1,9 +1,27 @@
 <script setup>
 
-import { ref } from "vue"; const color = ref()
+import { ref } from "vue"; const color = ref(transparent);
+const cambiarColor = () => { color.value = color.value === "transparent" ? "red" : "transparent";
+
+}
 
 </script>
 
 
 <template>
+    <div class="container">
+    <img src='/src/components/icons/Image20231205125028.png' alt="heart transp" />
+    <button @click="cambiarColor">
+      <img: src="color === 'transparent' ? '/src/components/icons/Image20231205125028.png' : '/src/components/icons/corazon.png'" alt="heart" />
+    </button>
+  </div>
+</template>
+
+  <style lang="scss" scoped>
+.container {
+  display: flex; 
+ 
+  align-items: center; 
+}
+</style>
 	
