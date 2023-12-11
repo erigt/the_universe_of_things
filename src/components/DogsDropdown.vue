d006fae532b87870b90bb4e9d12755463a909a7d
Natt83~ <natimetal83@gmail.com>
2023-12-11 14:59:09 +0100

    apicall-breeds

--- src/components/DogsDropdown.vue
+++ src/components/DogsDropdown.vue
@@ -1,6 +1,4 @@
-
 <script>
-
 export default {
   data() {
     return {
@@ -9,24 +7,61 @@ export default {
     };
   },
   mounted() {
-
     this.getDogBreeds();
   },
   methods: {
     async getDogBreeds() {
       try {
-
-        const response = await this.$axios.get('https://dog.ceo/api/breeds/list/all');
-
-
-        this.dogBreeds = response.data.map(breed => breed.name);
+        const apiDogs = new ApiDogs();
+        const dogBreeds = await apiDogs.getAll();
+        this.dogBreeds = dogBreeds.map(breed => breed.name);
       } catch (error) {
         console.error('Error al obtener las razas de perros:', error);
       }
     }
   }
 };
+
+class ApiDogs {
+  #Url;
+
+  constructor() {
+    this.#Url = 'https://dog.ceo/api/breeds/list/all';
+  }
+
+  async getAll() {
+    const response = await fetch(this.#Url);
+    const json = await response.json();
+
+    let charactersPure = [];
+    let charactersReturn = [];
+
+    
+    for (const breed in json.message) {
+      const breedResponse = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+      const breedJson = await breedResponse.json();
+
+      charactersPure.push({
+        name: breed,
+        image: breedJson.message
+      });
+    }
+
+    for (const character of charactersPure) {
+      const characterToAdd = {
+        name: character.name,
+        image: character.image,
+        
+      };
+
+      charactersReturn.push(characterToAdd);
+    }
+
+    return charactersReturn;
+  }
+}
 </script>
+
 <template>
   <div>
     <label for="dogBreed">Selecciona una raza :</label>
@@ -35,14 +70,14 @@ export default {
     </select>
   </div>
 </template>
+
 <style scoped>
 div {
   margin: 20px;
 }
 
-
 label {
- display: flex;
+  display: flex;
   margin-bottom: 8px;
 }
 
@@ -53,10 +88,9 @@ select {
   border: 1px solid #ccc;
   border-radius: 4px;
   box-sizing: border-box;
-
 }
 
-select :hover {
+select:hover {
   color: bisque;
 }
 </style>
