c80798d6f985b1628f1284aca712427e7add09c7
Matilde García Suárez <matigarcia0817@gmail.com>
2023-12-12 14:23:42 +0100

    getting better changes

--- src/components/CheckLoging.vue
+++ src/components/CheckLoging.vue
@@ -1,27 +1,69 @@
 <script setup>
 
-import { ref } from "vue"; const color = ref(transparent);
-const cambiarColor = () => { color.value = color.value === "transparent" ? "red" : "transparent";
+import { ref } from 'vue';
 
-}
+const corazonTransparente = ref(true);
 
+const cambiarColor = () => {
+  corazonTransparente.value = !corazonTransparente.value;
+};
 </script>
 
+<script setup>
+import { ref } from 'vue';
+
+const usuarioLogeado = ref(false);
+const corazonTransparente = ref(true);
+
+const manejarClicCorazon = () => {
+  if (usuarioLogeado.value) {
+    cambiarColor();
+  } else {
+    abrirFormulario();
+  }
+};
+
+const cambiarColor = () => {
+  corazonTransparente.value = !corazonTransparente.value;
+};
+
+const abrirFormulario = () => {
+  
+
+  console.log("Abrir formulario de inicio de sesión");
+};
+
+const corazonSrc = ref("/src/components/icons/Image20231205125028.png");
+
+// Actualiza la fuente del corazón según el estado de autenticación
+watchEffect(() => {
+  corazonSrc.value = corazonTransparente.value
+    ? "/src/components/icons/Image20231205125028.png"
+    : "/src/components/icons/corazon.png";
+});
+</script>
 
 <template>
-    <div class="container">
-    <img src='/src/components/icons/Image20231205125028.png' alt="heart transp" />
-    <button @click="cambiarColor">
-      <img: src="color === 'transparent' ? '/src/components/icons/Image20231205125028.png' : '/src/components/icons/corazon.png'" alt="heart" />
-    </button>
+<div>
+    <img
+      v-if="corazonTransparente"
+      :src="require('/src/components/icons/Image20231205125028.png')"
+      alt="Corazón Transparente"
+      @click="cambiarColor"
+    />
+    <img
+      v-else
+      :src="require('/src/components/icons/corazon.png')"
+      alt="Corazón Rojo"
+      @click="cambiarColor"
+    />
   </div>
+
 </template>
 
-  <style lang="scss" scoped>
-.container {
-  display: flex; 
- 
-  align-items: center; 
+<style>
+img {
+  width: 50px;
+  cursor: pointer;
 }
 </style>
-	
