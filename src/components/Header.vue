4b6fdd59261cc19f9bdcfcaf0d948abfef8a196e
Erika <146768635+erigt@users.noreply.github.com>
2023-12-13 14:45:42 +0100

    Changes Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,15 @@
 <script setup></script>
 <template>
-	<div>
+	<header>
 		
-	</div>
+
+		<nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/Login">Login</RouterLink>
+      <RouterLink to="/Favorites">Favorites</RouterLink>
+    </nav>
+
+	</header>
 </template>
 <style lang="scss" scoped>
 
